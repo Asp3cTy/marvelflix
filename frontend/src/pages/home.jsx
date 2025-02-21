10a2cddd4566916bce914c6822aaabca8bc69477
Julio Moriera <julinhopentakill@gmail.com>
2025-02-21 18:49:55 -0300

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -4,7 +4,6 @@ import { Link } from "react-router-dom";
 import axios from "axios";
 import API_URL from "../config"; 
 
-
 const Home = () => {
   const [collections, setCollections] = useState([]);
 
@@ -18,20 +17,26 @@ const Home = () => {
     <div className="bg-marvelDark min-h-screen text-white p-6">
       <h1 className="text-marvelRed text-4xl font-bold text-center">MarvelFlix</h1>
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 p-6">
-        {collections.map((collection) => (
-          <Link to={`/collection/${collection.id}`} key={collection.id}>
-            <div className="relative group overflow-hidden rounded-lg shadow-lg">
-              <img
-                src={collection.cover_url}
-                alt={collection.name}
-                className="w-full h-72 object-cover rounded-lg transition-transform duration-300 group-hover:scale-105"
-              />
-              <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                <h2 className="text-xl font-bold text-white text-center p-4">{collection.name}</h2>
+        {collections.map((collection) => {
+          const collectionImageUrl = collection?.image?.startsWith("http")
+            ? collection.image  
+            : `${API_URL}${collection.image}`;
+
+          return (
+            <Link to={`/collection/${collection.id}`} key={collection.id}>
+              <div className="relative group overflow-hidden rounded-lg shadow-lg">
+                <img
+                  src={collectionImageUrl}
+                  alt={collection.name}
+                  className="w-full h-72 object-cover rounded-lg transition-transform duration-300 group-hover:scale-105"
+                />
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                  <h2 className="text-xl font-bold text-white text-center p-4">{collection.name}</h2>
+                </div>
               </div>
-            </div>
-          </Link>
-        ))}
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
