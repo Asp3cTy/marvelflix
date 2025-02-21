d7921818ad7269ff3aa9ab465a0bffcea13d9878
Julio Moriera <julinhopentakill@gmail.com>
2025-02-21 02:06:46 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: "0.0.0.0", // Permite acesso externo no Render
-    port: process.env.PORT || 10000, // Usa a porta do Render ou 10000 como fallback
+  preview: {
+    allowedHosts: ["marvelflix.onrender.com", "0.0.0.0"], // Permitir acesso pelo domínio no Render
   },
-});
+  server: {
+    host: true, // Permite acesso a partir de qualquer IP externo
+    port: 4173, // Garanta que está usando a porta correta
+  },
+})
