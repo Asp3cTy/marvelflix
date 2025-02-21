cddac13320da20463509ce4142065f01f12c9f0d
Julio Moriera <julinhopentakill@gmail.com>
2025-02-21 18:34:58 -0300

    Update collectionview.jsx

--- frontend/src/pages/collectionview.jsx
+++ frontend/src/pages/collectionview.jsx
@@ -29,7 +29,7 @@ const CollectionView = () => {
 
     return (
         <div className="p-10 text-white text-center bg-marvelDark min-h-screen">
-            <div className="fixed top-4 left-4 mb-10">
+            <div className="sticky top-2 left-0 flex justify-start z-50 mb-4">
                 <button 
                     onClick={() => window.location.href = "/"} 
                     className="bg-red-600 text-white px-4 py-2 rounded-lg shadow-lg hover:bg-red-700 transition">
