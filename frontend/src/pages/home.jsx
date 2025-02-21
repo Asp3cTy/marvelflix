0c0fce2f056a174539f3f2d4aa2c60dabfda6bbe
Julio Moriera <julinhopentakill@gmail.com>
2025-02-21 19:01:03 -0300

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -28,7 +28,7 @@ const Home = () => {
                 <img
                   src={collectionImageUrl}
                   alt={collection.name}
-                  className="w-full h-72 object-cover rounded-lg transition-transform duration-300 group-hover:scale-105"
+                  className="w-full h-[400px] md:h-[450px] lg:h-[500px] object-cover rounded-lg transition-transform duration-300 group-hover:scale-105"
                 />
                 <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                   <h2 className="text-xl font-bold text-white text-center p-4">{collection.name}</h2>
