eac06ce097407568b0f03deea3584e8e0b34d6c0
Julio Moriera <julinhopentakill@gmail.com>
2025-02-21 01:41:36 -0300

    Update login.jsx

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { AuthContext } from "../context/authcontext";
 import { useNavigate } from "react-router-dom";
 
-const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+const API_URL = import.meta.env.VITE_API_URL;
 
 
 const Login = () => {
