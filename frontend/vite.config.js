a5d5291d50107127d356b20c7ef5b08d09ec279a
Julio Moriera <julinhopentakill@gmail.com>
2025-02-21 02:03:15 -0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: "0.0.0.0", // Permite acesso externo no Render
+    port: process.env.PORT || 10000, // Usa a porta do Render ou 10000 como fallback
+  },
+});
