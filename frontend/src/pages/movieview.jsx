cc36e55512a574ea38a5d6212844179f9870978d
Julio Moriera <julinhopentakill@gmail.com>
2025-02-21 01:46:24 -0300

    Update movieview.jsx

--- frontend/src/pages/movieview.jsx
+++ frontend/src/pages/movieview.jsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-
-const API_URL = import.meta.env.VITE_API_URL;
+import API_URL from "../config"; 
 
 
 const MovieView = () => {
