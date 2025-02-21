4f16ed8fcf69a420ebe574c11b343f07aa00e478
Julio Moriera <julinhopentakill@gmail.com>
2025-02-21 01:46:04 -0300

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,8 +2,7 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-
-const API_URL = import.meta.env.VITE_API_URL;
+import API_URL from "../config"; 
 
 
 const Home = () => {
