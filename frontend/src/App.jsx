d8560b828358c32126dc2a2f31cd01bcba97ad0c
Julio Moriera <julinhopentakill@gmail.com>
2025-02-22 01:49:00 -0300

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import { AuthProvider, AuthContext } from "./context/authcontext";
 import Header from "./components/header";
+import Footer from "./components/footer";
 import Home from "./pages/home";
 import CollectionView from "./pages/collectionview";
 import MovieView from "./pages/movieview";
