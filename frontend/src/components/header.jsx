f270ff2acaa392f2fddf00a222eb11456789668f
Julio Moriera <julinhopentakill@gmail.com>
2025-02-22 10:11:16 -0300

    Update header.jsx
    Alterando Header para Fixo

--- frontend/src/components/header.jsx
+++ frontend/src/components/header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
 
   return (
     <>
-      {/* Header Principal */}
-      <header className="bg-marvelDark text-white p-4 shadow-md relative z-50">
+      {/* Header Fixo no Topo */}
+      <header className="fixed top-0 left-0 w-full bg-marvelDark text-white p-4 shadow-md z-50">
         <div className="container mx-auto flex items-center justify-between">
           {/* Botão Mobile */}
           <button 
@@ -25,14 +25,13 @@ const Header = () => {
           </nav>
 
           {/* Logo Centralizada no Desktop */}
-              <Link to="/" className="absolute left-1/2 transform -translate-x-1/2">
-                <img 
-                  src="https://i.imgur.com/GpB2cuj.png" 
-                  alt="MarvelFlix" 
-                 className="h-12 md:h-14 lg:h-16 w-auto mx-auto"
-                />
-              </Link>
-
+          <Link to="/" className="absolute left-1/2 transform -translate-x-1/2">
+            <img 
+              src="https://i.imgur.com/GpB2cuj.png" 
+              alt="MarvelFlix" 
+              className="h-12 md:h-14 lg:h-16 w-auto mx-auto"
+            />
+          </Link>
 
           {/* Botão de Login */}
           <Link to="/login" className="bg-red-600 px-4 py-2 rounded-lg hover:bg-red-700 transition">
@@ -43,7 +42,7 @@ const Header = () => {
 
       {/* Menu Mobile (Dropdown) */}
       {isMenuOpen && (
-        <div className="bg-marvelDark text-white py-4 shadow-md">
+        <div className="bg-marvelDark text-white py-4 shadow-md mt-16">
           <nav className="flex flex-col items-center space-y-4">
             <Link to="/" className="hover:text-marvelRed transition">Home</Link>
             <Link to="/collections" className="hover:text-marvelRed transition">Coleções</Link>
@@ -51,6 +50,9 @@ const Header = () => {
           </nav>
         </div>
       )}
+
+      {/* Adiciona um espaçamento para que o conteúdo não fique atrás do Header */}
+      <div className="pt-20"></div>
     </>
   );
 };
