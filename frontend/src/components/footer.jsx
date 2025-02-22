7f8a41a87c92928102b43061a1604c25e9b40250
Julio Moriera <julinhopentakill@gmail.com>
2025-02-22 09:09:17 -0300

    Update footer.jsx

--- frontend/src/components/footer.jsx
+++ frontend/src/components/footer.jsx
@@ -4,7 +4,7 @@ import { FaTelegramPlane, FaWhatsapp } from "react-icons/fa";
 const Footer = () => {
   return (
     <footer className="bg-marvelDark text-white py-6 mt-10">
-      <div className="container mx-auto flex flex-col items-center justify-center text-center space-y-4">
+      <div className="container mx-auto flex flex-col items-center text-center space-y-4">
         
         {/* Texto de direitos autorais */}
         <p className="text-sm">© 2025 MarvelFlix. Todos os direitos reservados.</p>
@@ -18,12 +18,12 @@ const Footer = () => {
         </button>
 
         {/* Ícones de Redes Sociais */}
-        <div className="flex flex-col items-center space-y-2 md:space-y-0 md:flex-col md:items-end w-full md:w-auto">
+        <div className="flex flex-col space-y-2 items-center mt-4">
           <a href="https://t.me/seuTelegram" target="_blank" rel="noopener noreferrer">
-            <FaTelegramPlane className="text-white-400 text-2xl hover:text-white-500 transition" />
+            <FaTelegramPlane className="text-white-400 text-3xl hover:text-white-500 transition" />
           </a>
           <a href="https://wa.me/seuNumero" target="_blank" rel="noopener noreferrer">
-            <FaWhatsapp className="text-white-400 text-2xl hover:text-white-500 transition" />
+            <FaWhatsapp className="text-white-400 text-3xl hover:text-white-500 transition" />
           </a>
         </div>
 
