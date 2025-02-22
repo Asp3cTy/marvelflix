475477e71a20a38202b22f04904b3dd7ce08c6c0
Julio Moriera <julinhopentakill@gmail.com>
2025-02-22 00:29:20 -0300

    Update home.jsx

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
   return (
     <div className="bg-marvelDark min-h-screen text-white p-6">
       <h1 className="text-marvelRed text-4xl font-bold text-center">MarvelFlix</h1>
-      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6 p-6">
+      <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6 p-6">
         {collections.map((collection) => {
           const collectionImageUrl = collection?.image?.startsWith("http")
             ? collection.image  
@@ -28,7 +28,7 @@ const Home = () => {
                 <img
                   src={collectionImageUrl}
                   alt={collection.name}
-                  className="w-full h-[400px] md:h-[450px] lg:h-[500px] object-cover rounded-lg transition-transform duration-300 group-hover:scale-105"
+                className="w-full h-auto object-cover rounded-lg transition-transform duration-300 group-hover:scale-105"
                 />
                 <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
                   <h2 className="text-xl font-bold text-white text-center p-4">{collection.name}</h2>
