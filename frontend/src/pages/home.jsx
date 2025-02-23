9ef1a1d68e8f0d4da9596ea215d4745184efb06a
Asp3cTy <julinhopentakill@gmail.com>
2025-02-22 23:05:45 -0300

    Implementando Logout no Header

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -8,11 +8,6 @@ const Home = () => {
       <p className="text-lg md:text-2xl max-w-2xl text-center mb-8">
         Descubra e assista a uma vasta coleção de filmes e séries do universo Marvel. Explore nossas coleções e encontre seus favoritos!
       </p>
-      <img
-        src="https://i.imgur.com/GpB2cuj.png"
-        alt="MarvelFlix"
-        className="h-24 md:h-32 lg:h-40 w-auto mx-auto mb-8"
-      />
       <button className="bg-red-600 px-6 py-3 rounded-lg text-lg md:text-xl font-semibold hover:bg-red-700 transition">
         Explorar Coleções
       </button>
