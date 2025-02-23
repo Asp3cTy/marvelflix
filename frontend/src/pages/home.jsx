a7a41ceec13aca5edb64e53af40af5d55cbc15fa
Asp3cTy <julinhopentakill@gmail.com>
2025-02-22 23:27:18 -0300

    Implementando Logout no Header

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -1,17 +1,19 @@
-// src/pages/home.jsx
-import React from "react";
-
 const Home = () => {
   return (
     <div className="bg-marvelDark min-h-screen text-white relative flex flex-col items-center justify-center">
       <div className="absolute inset-0 bg-cover bg-center opacity-70" style={{ backgroundImage: 'url("https://i.postimg.cc/gj9y4WYc/bg-home-upscayl-3x-high-fidelity-4x.png")' }}></div>
-      <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="relative z-10 p-6">
+      <div className="absolute inset-0 bg-marvelDark opacity-70"></div>
+      <div className="relative z-10 p-6 text-center">
         <h1 className="text-4xl md:text-6xl font-bold mb-4">Bem-vindo ao MarvelFlix!</h1>
-        <p className="text-lg md:text-2xl max-w-2xl text-center mb-8">
+        <p className="text-lg md:text-2xl max-w-2xl mx-auto mb-8">
           Descubra e assista a uma vasta coleção de filmes e séries do universo Marvel. Explore nossas coleções e encontre seus favoritos!
         </p>
-        <button className="bg-red-600 px-6 py-3 rounded-lg text-lg md:text-xl font-semibold hover:bg-red-700 transition">
+        <img
+          src="https://i.imgur.com/GpB2cuj.png"
+          alt="MarvelFlix"
+          className="h-24 md:h-32 lg:h-40 w-auto mx-auto mb-8"
+        />
+        <button className="bg-red-600 px-6 py-3 rounded-lg text-lg md:text-xl font-semibold hover:bg-red-700 transition mx-auto">
           Explorar Coleções
         </button>
       </div>
