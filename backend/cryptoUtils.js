80c372767eb82474e99e9a4101278d9a2a066bf3
Asp3cTy <julinhopentakill@gmail.com>
2025-02-23 00:28:16 -0300

    Implementando Logout no Header

--- backend/cryptoUtils.js
+++ backend/cryptoUtils.js
@@ -1,10 +1,14 @@
 const crypto = require('crypto');
 const algorithm = 'aes-256-cbc';
-const key = process.env.CRYPTO_KEY; // A chave deve ter 32 bytes
+const key = Buffer.from('SUA_CHAVE_BASE64', 'base64'); // Substitua 'SUA_CHAVE_BASE64' pela chave gerada
 const iv = crypto.randomBytes(16);
 
+if (key.length !== 32) {
+  throw new Error('Chave de criptografia inválida. A chave deve ter 32 bytes.');
+}
+
 const encrypt = (text) => {
-  let cipher = crypto.createCipheriv(algorithm, Buffer.from(key), iv);
+  let cipher = crypto.createCipheriv(algorithm, key, iv);
   let encrypted = cipher.update(text);
   encrypted = Buffer.concat([encrypted, cipher.final()]);
   return iv.toString('hex') + ':' + encrypted.toString('hex');
@@ -14,7 +18,7 @@ const decrypt = (text) => {
   let textParts = text.split(':');
   let iv = Buffer.from(textParts.shift(), 'hex');
   let encryptedText = Buffer.from(textParts.join(':'), 'hex');
-  let decipher = crypto.createDecipheriv(algorithm, Buffer.from(key), iv);
+  let decipher = crypto.createDecipheriv(algorithm, key, iv);
   let decrypted = decipher.update(encryptedText);
   decrypted = Buffer.concat([decrypted, decipher.final()]);
   return decrypted.toString();
