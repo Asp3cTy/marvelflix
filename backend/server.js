dacb7dbab5f1682cac049544377e3ec24712a944
Asp3cTy <julinhopentakill@gmail.com>
2025-02-23 02:04:45 -0300

    Testing-5

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,13 @@ const moviesRoutes = require("./routes/movies");
 const thumbnailsRoutes = require("./routes/thumbnails");
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: false
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
