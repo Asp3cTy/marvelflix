befcd44a2387f4e209621b894b0d7064194f6d0f
Julio Moriera <julinhopentakill@gmail.com>
2025-02-24 17:10:45 -0300

    Update collections.jsx

--- frontend/src/pages/collections.jsx
+++ frontend/src/pages/collections.jsx
@@ -8,7 +8,8 @@ const Collections = () => {
   const [collections, setCollections] = useState([]);
 
   useEffect(() => {
-    axios.get(`${API_URL}/api/collections`)
+    axios
+      .get(`${API_URL}/api/collections`)
       .then((response) => setCollections(response.data))
       .catch((error) => console.error("Erro ao buscar coleções:", error));
   }, []);
@@ -17,6 +18,7 @@ const Collections = () => {
     <div className="bg-marvelDark min-h-screen text-white p-6">
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-6 p-6">
         {collections.map((collection) => {
+          // Se 'collection.image' não for link absoluto, prefixa com API_URL
           const collectionImageUrl = collection?.image?.startsWith("http")
             ? collection.image
             : `${API_URL}${collection.image}`;
@@ -29,8 +31,7 @@ const Collections = () => {
                   alt={collection.name}
                   className="w-full h-auto object-cover rounded-lg transition-transform duration-300 group-hover:scale-105"
                 />
-                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-                </div>
+                <div className="absolute inset-0 bg-black bg-opacity-50 flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
               </div>
             </Link>
           );
